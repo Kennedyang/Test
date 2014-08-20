abd1da745d0aeb1f1dd86c0fc521673b60191fe0
kyang <kyang@expedia.com>
2014-08-20 16:24:58 +0800

    test

---
+++ Common/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Utility u = new Utility();
+            Assert.AreEqual(7, u.sum(3, 4));
+        }
+    }
+}
